c4865dfa71e8c48d505704facf41973a172a705f
mi <mi@localhost.localdomain>
2017-07-19 18:11:34 +0200

    formation+offre

--- private/view/offer-cards.php
+++ private/view/offer-cards.php
@@ -1,8 +1,11 @@
 <div class="row no-margin-bottom">
 			<div class="budget col s12 m6 l4">
-				<div class="card"; >
-					<div class="card-content z-depth-2">
-						<h1>Offre #1</h1>
+				<div class="card z-depth-2"; >
+					<div class="offer-card valign-wrapper  " id="starter">
+							<a href=""  class="cta card-header  center-align"><h2 class="valign-wrapper">#1 L'essentiel</h2></a>
+					</div>
+					<div class="card-content ">
+					
 						<p style="color: gray;">Lorem ipsum dolor sit amet. Vitae et consectetuer. Vivamus sagittis ante auctor nascetur ve. Pellentesque. Eget vestibulum amet metus pretium id, porttitor mi. Consequat class tristique turpis vehicula enim, nam. Quisque id arcu ullamcorper quisque, scelerisque erat arcu, congue. Venenatis suscipit ornare natoque tellus nibh ac sollicitudin viverra, non. Cum quis lorem suspendisse faucibus diam vel. In sociis consectetuer consequat magnis platea 
 </p>
 					</div>
@@ -10,9 +13,12 @@
 				</div>
   			</div>
   			<div class="budget col s12 m6 l4">
-				<div class="card" >
-					<div class="card-content z-depth-2">
-						<h1>Offre #2</h1>
+				<div class="card z-depth-2" >
+					
+					<div class="offer-card valign-wrapper  " id="bases">
+						<a href=""  class="cta card-header  center-align"><h2 class="valign-wrapper">#2 Les bonnes bases</h2></a>
+					</div>
+					<div class="card-content ">
 						<p style="color: gray;">Lorem ipsum dolor sit amet. Vitae et consectetuer. Vivamus sagittis ante auctor nascetur ve. Pellentesque. Eget vestibulum amet metus pretium id, porttitor mi. Consequat class tristique turpis vehicula enim, nam. Quisque id arcu ullamcorper quisque, scelerisque erat arcu, congue. Venenatis suscipit ornare natoque tellus nibh ac sollicitudin viverra, non. Cum quis lorem suspendisse faucibus diam vel. In sociis consectetuer consequat magnis platea
 </p>
 					</div>
@@ -20,9 +26,12 @@
 				</div>
   			</div>
   			<div class="budget col s12 m6 l4">
-				<div class="card"; >
-					<div class="card-content z-depth-2">
-						<h1>Offre #3</h1>
+				<div class="card z-depth-2" >
+					
+					<div class="offer-card valign-wrapper  " id="total">
+						<a href=""  class="cta card-header  center-align"><h2 class="valign-wrapper">#3 La totale</h2></a>
+					</div>
+						<div class="card-content ">
 						<p style="color: gray;">Lorem ipsum dolor sit amet. Vitae et consectetuer. Vivamus sagittis ante auctor nascetur ve. Pellentesque. Eget vestibulum amet metus pretium id, porttitor mi. Consequat class tristique turpis vehicula enim, nam. Quisque id arcu ullamcorper quisque, scelerisque erat arcu, congue. Venenatis suscipit ornare natoque tellus nibh ac sollicitudin viverra, non. Cum quis lorem suspendisse faucibus diam vel. In sociis consectetuer consequat magnis platea
 </p>
 					</div>
