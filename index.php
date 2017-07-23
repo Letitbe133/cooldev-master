649e1242467695f1e397b5519a96f8242e86fbcd
mi <mi@localhost.localdomain>
2017-07-23 12:28:31 +0200

    cards offre

--- index.php
+++ index.php
@@ -269,7 +269,7 @@ En diminuant le temps de développement, <mark>nous pouvons rendre abordable ce 
               <div id="modal1" class="modal modal-fixed-footer">
                 <div class="modal-content">
                   <h3>SIRET</h3>
-                  <p>Sous CAPE Cosens SIRET 419 369 798 00048</p>
+                  <p>Sous CAPE Cosens <br>SIRET : 419 369 798 00048<br>N° Formateur : 93 13 113 41 13</p>
                   <h3>Hébergement :</h3>
                   <p>GANDI SAS, Société par Actions Simplifiée au capital de 300.000€ ayant son siège social au 63-65 boulevard Massena à Paris (75013) FRANCE, immatriculée sous le numéro 423 093 459 RCS PARIS N° TVA FR81423093459 Téléphone : +33.(0) 1 70.37.76.61 . Ce dernier est en possession des coordonées de l'éditeur et du responsable de la publication (merci gandi!)</p>
                   <h3>Gestion des données personnelles :</h3>
--- private/view/offer-cards.php
+++ private/view/offer-cards.php
@@ -2,25 +2,26 @@
 			<div class="budget col s12 m6 l4">
 				<div class="card z-depth-2"; >
 					<div class="offer-card valign-wrapper  " id="starter">
-							<a href=""  class="cta card-header  center-align"><h2 class="valign-wrapper">#1 L'essentiel</h2></a>
+							<a href=""  class="cta card-header  center-align"><img src="TinySite/assets/logo-white.png" alt="" width=""></a>
 					</div>
 					<div class="card-content ">
 					<ul class="browser-default">
-						<li>Une landing-page au design actuel avec toutes vos infos importantes</li>
+						<li><a href="TinySite"> Une landing-page au design actuel avec toutes vos infos importantes</a></li>
 						<li>Le nom de domaine en .fr pendant 1 an</li>
 						<li>L'hébergement pendant 1 an</li>	
 								
 					</ul>	
 					</p>
 					</div>
-					<div class="card-action z-depth-2 "><h2>XXX €</h2>	</div>				
+					<div class="card-action z-depth-2 "><h2>349 € ht</h2>	</div>				
 				</div>
   			</div>
   			<div class="budget col s12 m6 l4">
 				<div class="card z-depth-2" >
 					
 					<div class="offer-card valign-wrapper  " id="bases">
-						<a href=""  class="cta card-header  center-align"><h2 class="valign-wrapper">#2 Les bonnes bases</h2></a>
+						<a href=""  class="cta card-header center-align"><img  src="assets/img/tiny-site+.png" alt="" width=""></a>
+					
 					</div>
 					<div class="card-content ">
 						<p style="color: gray;">Lorem ipsum dolor sit amet. Vitae et consectetuer. Vivamus sagittis ante auctor nascetur ve. Pellentesque. Eget vestibulum amet metus pretium id, porttitor mi. Consequat class tristique turpis vehicula enim, nam. Quisque id arcu ullamcorper quisque, scelerisque erat arcu, congue. Venenatis suscipit ornare natoque tellus nibh ac sollicitudin viverra, non. Cum quis lorem suspendisse faucibus diam vel. In sociis consectetuer consequat magnis platea
@@ -33,7 +34,7 @@
 				<div class="card z-depth-2" >
 					
 					<div class="offer-card valign-wrapper  " id="total">
-						<a href=""  class="cta card-header  center-align"><h2 class="valign-wrapper">#3 La totale</h2></a>
+						<a href=""  class="cta card-header  center-align"><img  src="assets/img/logo-surmesure.png" alt="" width=""></a></a>
 					</div>
 						<div class="card-content ">
 						<p style="color: gray;">Lorem ipsum dolor sit amet. Vitae et consectetuer. Vivamus sagittis ante auctor nascetur ve. Pellentesque. Eget vestibulum amet metus pretium id, porttitor mi. Consequat class tristique turpis vehicula enim, nam. Quisque id arcu ullamcorper quisque, scelerisque erat arcu, congue. Venenatis suscipit ornare natoque tellus nibh ac sollicitudin viverra, non. Cum quis lorem suspendisse faucibus diam vel. In sociis consectetuer consequat magnis platea
